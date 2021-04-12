7fcf57f2e1fc50b93659e7fb6d51c5f609543545
Joanna Kozłowska <joakoz92@wp.pl>
2021-04-12 22:06:17 +0200

    exercise about shop online is added

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SKU = Stock Keeping Unit
+            string sku = "01-MN-L";
+
+            string[] product = sku.Split('-');
+
+            string type = "";
+            string color = "";
+            string size = "";
+            int number = int.Parse(product[0]);
+            switch (number)
+            {
+                case 1:
+                    type = "Sweat shirt";
+                    break;
+
+                case 2:
+                    type = "T-Shirt";
+                    break;
+
+                case 3:
+                    type = "sWEET PANTS";
+                    break;
+                default:
+                    type = "other";
+                    break;
+            }
+
+            switch (product[2])
+            {
+                case "MN":
+                    color = "black";
+                    break;
+
+                case "BL":
+                    color = "Maroon";
+                    break;
+
+                default:
+                    color = "White";
+                    break;
+            }
+            switch (product[2])
+            {
+                case "S":
+                    size = "small";
+                    break;
+
+                case "M":
+                    size = "Medium";
+                    break;
+
+                default:
+                    size = "Large";
+                    break;
+            }
+            Console.WriteLine($"Product: {size} {color} {type}");
+        }
+    }
+}
